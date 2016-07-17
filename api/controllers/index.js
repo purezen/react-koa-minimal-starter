b0c2c7dcc20cc7fc64c572c1407a344bf8c6dc96
purezen_ <aditya2204@gmail.com>
2016-07-17 17:22:56 +0530

    typo

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,12 +1,12 @@
 import glob from 'glob'
 
-export default function attachControllers(router) {
+export default function attachControllers (router) {
   const controllers = glob.sync('*.js', {
                         ignore: ['index.js'],
                         cwd: process.cwd() + '/api/controllers'
                       })
 
-  controllers.map(function (controller) {
+  controllers.map (function (controller) {
     require('./' + controller).default(router)
   })
 }
