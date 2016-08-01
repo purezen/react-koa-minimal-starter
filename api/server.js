15044ac19939db5723ad489867aa809623ed8512
purezen_ <aditya2204@gmail.com>
2016-08-01 15:38:27 +0530

    enable deploy to Heroku

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
+import path from 'path'
 import Koa from 'koa'
 import Router from 'koa-router'
 import attachControllers from './controllers'
 import cors from 'kcors'
+import serve from 'koa-static'
 
 const app = new Koa()
 const router = new Router({
@@ -9,8 +11,11 @@ const router = new Router({
 })
 
 app.use(cors())
-
 attachControllers(router)
 
 app.use(router.routes())
+if (process.env.NODE_ENV === 'production') {
+  app.use(serve(path.join(__dirname,'/../build')))
+}
+
 app.listen(3000)
