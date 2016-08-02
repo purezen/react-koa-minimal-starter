b0818b195c1eef6c1e96b3b76e7dfa7dfcb9edea
purezen_ <aditya2204@gmail.com>
2016-08-02 14:32:15 +0530

    webpack production config improvements

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,23 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var loaders = [{
+  test: /\.js$/,
+  loader: 'babel',
+  query: {
+    "presets": ["es2015-node5", "stage-3", "react"],
+    "plugins": ["react-hot-loader/babel"]
+  },
+  include: path.join(__dirname, 'web')
+}];
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+];
+
 module.exports = {
   devtool: 'source-map',
   entry: './web/index',
@@ -8,15 +25,8 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
   },
+  plugins: plugins,
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      query: {
-        "presets": ["es2015-node5", "stage-0", "react"],
-        "plugins": ["react-hot-loader/babel"]
-      },
-      include: path.join(__dirname, 'web')
-    }]
+    loaders: loaders
   }
 };
